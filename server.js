db1a5db6f6f7f1e3e0b36424e01df91d07ef96eb
Anil Patel <ajpatel92@icloud.com>
2018-03-14 14:13:35 -0500

    packages, first route set up

--- server.js
+++ server.js
@@ -4,3 +4,19 @@ var cookieParser = require('cookie-parser');
 var methodOverride = require('method-override');
 var session = require('express-session');
 var ejs = require('ejs');
+var mysql = require("mysql");
+
+var app = express();
+var PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", function(req, res) {
+  console.log("hit!!");
+});
+
+app.listen(PORT, function(err){
+    if (err) throw err
+    console.log("Listening on port: " + PORT);
+});
