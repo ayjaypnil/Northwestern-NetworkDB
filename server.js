7b2c417de23253caf37998976c20d48bc1f9801d
Anil Patel <ajpatel92@icloud.com>
2018-03-14 13:49:31 -0500

    packages

---
+++ server.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var methodOverride = require('method-override');
+var session = require('express-session');
+var ejs = require('ejs');
