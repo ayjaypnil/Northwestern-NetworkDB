945e057e47c23e5a82624ba5160461fb9efc68cf
Anil Patel <ajpatel92@icloud.com>
2018-03-15 15:01:27 -0500

    mysql db

--- server.js
+++ server.js
@@ -13,8 +13,39 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.set("view engine", "ejs"); 
-
 app.use(express.static("public"));
+
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+
+//   // Your username
+//   user: "root",
+
+//   // Your password
+//   password: "",
+//   database: "nwusers_db"
+// });
+
+// connection.connect(function(err) {
+//   if (err) throw err;
+//   console.log("connected as id " + connection.threadId + "\n");
+//   connection.query("SELECT * FROM users_info", function(err, rows) {
+//     if (err) throw err;
+
+//     console.log("Data received from Db:\n");
+//     console.log(rows);
+//   });
+// });
 
 app.get("/", function(req, res) {
  res.render( __dirname + "/views/index.ejs");
