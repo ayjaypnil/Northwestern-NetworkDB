f510bf9b1714a892fd52d1b906a40455ad08f5dc
Anil Patel <ajpatel92@icloud.com>
2018-03-15 11:42:59 -0500

    index file

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
- res.render( __dirname + "/views/home.ejs");
+ res.render( __dirname + "/views/index.ejs");
 });
 
 app.get("/home", function(req, res) {
